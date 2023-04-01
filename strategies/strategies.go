a18c36b2d21532bf9a0c3b1513a017f3cf49d446
daydoing <daydoing@blockchain.org>
2023-04-01 21:02:48 +0800

    change grid mum

--- strategies/strategies.go
+++ strategies/strategies.go
@@ -16,7 +16,7 @@ func NewStrategy(strategy string) (strategy.Strategy, error) {
 	case EMA:
 		return NewCrossEMA(), nil
 	case TROUGH:
-		return NewTrough("5m", 9, 1.0, 3.0, 3.0), nil
+		return NewTrough("5m", 9, 3.0, 3.0, 3.0), nil
 	default:
 		return nil, errors.New("unsupported trading strategy")
 	}
